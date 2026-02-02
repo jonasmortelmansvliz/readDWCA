1881ad1fae85ea064fd08093a4bdabf4e0d33806
Jonas Mortelmans <jonas.mortelmans@vliz.be>
2026-02-02 15:27:37 +0100

    com

--- config/config_template.R
+++ config/config_template.R
@@ -4,8 +4,6 @@
 # Base project directory 
 BASE_DIR <- "~"
 
-# Input
-ZIP_DWCA_ZOOPLANKTON <- file.path(BASE_DIR, "data/dwca-lifewatch_zooplankton-v1.71.zip")
 
 # Working directories
 EXTRACT_DIR  <- file.path(BASE_DIR, "temp-extract")
