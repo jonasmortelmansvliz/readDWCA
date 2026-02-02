6f6f94cfdbd04560398fbbf35f3dea922804483b
Jonas Mortelmans <jonas.mortelmans@vliz.be>
2026-02-02 15:05:53 +0100

    trdy

---
+++ config/config_template.R
@@ -0,0 +1,12 @@
+#### PATH CONFIGURATION FILE
+#### AUTHOR: Jonas Mortelmans
+
+# Base project directory 
+BASE_DIR <- "~"
+
+# Input
+ZIP_DWCA_ZOOPLANKTON <- file.path(BASE_DIR, "data/dwca-lifewatch_zooplankton-v1.71.zip")
+
+# Working directories
+EXTRACT_DIR  <- file.path(BASE_DIR, "temp-extract")
+OUTPUT_DIR    <- file.path(BASE_DIR, "output")
